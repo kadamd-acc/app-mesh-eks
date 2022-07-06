f98f700c87bb776422b69158e54475ee7fc72f67
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-07-06 23:12:26 +0530

    eks name

--- modules/eks/variable.tf
+++ modules/eks/variable.tf
@@ -11,9 +11,9 @@ variable "private_subnets" {
   description = "List of private subnet IDs"
 }
 
-variable "public_subnets" {
-  description = "List of private subnet IDs"
-}
+# variable "public_subnets" {
+#   description = "List of private subnet IDs"
+# }
 
 variable "fargate_app_namespace" {
   description = "Name of fargate selector namespace"
